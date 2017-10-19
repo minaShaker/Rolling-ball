dbdeb8a5a9f3319c9a7332b0001784612d19d665
mina shaker <minashaker64@yahoo.com>
2017-10-19 02:04:34 +0200

    Add files

---
+++ Assets/scripts/cameraControler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControler : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after all other items has prccessed in update so the camera will change after 
+    // the ball finished moving
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+
+	}
+}
---
+++ Assets/scripts/playerControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerControler : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    
+
+    private int count;
+    public Text countText;
+    public Text winText;
+    //called before rendring a frame game code go there
+    void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Count: " + count.ToString();
+        if (count >= 48)
+        {
+            winText.text = "You Win!";
+        }
+        winText.text = ""; 
+    }
+
+    //called before any physics calculation physics code here (forces here)
+    void FixedUpdate () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(h, 0, v);
+        rb.AddForce(movement *speed);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            countText.text = "Count: " + count.ToString();
+            if (count >= 48)
+        {
+            winText.text = "You Win!";
+        }
+        }
+    }
+}
